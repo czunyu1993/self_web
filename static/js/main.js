aadade87ff5965ffed98a487345f18c7fe33bf2d
Zunyu Chen <chzy1993@hotmail.com>
2016-11-18 05:57:06 -0500

    change margin of nav li perfectly

--- static/js/main.js
+++ static/js/main.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
     $('.nav-ul-li > a').hover(
         // handle in
         function() {
-            $(this).filter(':not(:animated)').animate({color:'#339966'}, 200);
+            $(this).filter(':not(:animated)').animate({color:'#339966'}, 120);
         },
         // handle out
         function() {
-            $(this).animate({color:'#2F2F2F'}, 200);
+            $(this).animate({color:'#2F2F2F'}, 120);
         }
     );
 });
