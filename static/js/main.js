7db38e62c46089daa087d7368ce4a71820d95aab
Zunyu Chen <chzy1993@hotmail.com>
2016-11-18 05:18:17 -0500

    positioned nav and di some animation

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,14 @@
+console.log("Hello");
 
-
-console.log("Hello, EECS485")
+$(document).ready(function() {
+    $('.nav-ul-li > a').hover(
+        // handle in
+        function() {
+            $(this).filter(':not(:animated)').animate({color:'#339966'}, 200);
+        },
+        // handle out
+        function() {
+            $(this).animate({color:'#2F2F2F'}, 200);
+        }
+    );
+});
