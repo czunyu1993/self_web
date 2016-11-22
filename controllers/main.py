9fbc4e68d182332163695036dde9112e3ab572c0
Zunyu Chen <chzy1993@hotmail.com>
2016-11-22 15:39:28 -0500

    initialized all templates and did icon hover zooming

--- controllers/main.py
+++ controllers/main.py
@@ -7,5 +7,17 @@ def main_route():
     return render_template("index.html")
 
 @main.route('/works')
-def experience_route():
+def works_route():
     return render_template("works.html")
+
+@main.route('/hobbies')
+def hobbies_route():
+    return render_template("hobbies.html")
+
+@main.route('/resume')
+def resume_route():
+    return render_template("resume.html")
+
+@main.route('/contact')
+def contact_route():
+    return render_template("contact.html")
