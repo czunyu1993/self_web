f4f9b9e8334aa0976857f00e969f68544d28dc80
Zunyu Chen <chzy1993@hotmail.com>
2016-11-21 20:51:14 -0500

    changed icon and new sidenav in mobile view

--- controllers/main.py
+++ controllers/main.py
@@ -5,3 +5,7 @@ main = Blueprint('main', __name__, template_folder='templates')
 @main.route('/')
 def main_route():
     return render_template("index.html")
+
+@main.route('/works')
+def experience_route():
+    return render_template("works.html")
--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,21 @@
 console.log("Hello");
 
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    // $("#body-div").css("margin-left", "250px");
+    $("#mySidenav").css("width", "250px");
+    // $("#body-div").css("background-color", "rgba(0,0,0,0.4)");
+    $("#body-div").css("opacity", "0.5");
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    $("#mySidenav").css("width", "0");
+    // $("#body-div").css("margin-left", "0");
+    // $("#body-div").css("background-color", "white");
+    $("#body-div").css("opacity", "1");
+}
+
 $(document).ready(function() {
     $('.nav-ul-li > a').hover(
         // handle in
